b5a1ed873d67246d1ff4c5c7abd190bf55b37cb1
BelicusBr <X70r@outlook.com>
2023-03-18 21:34:21 -0300

    #Update-vs2.6.0

---
+++ Editor/Attributes/ReadOnlyVarAttributeDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using Cobilas.Unity.Utility;
+
+namespace Cobilas.Unity.Editor.Utility {
+    [CustomPropertyDrawer(typeof(ReadOnlyVarAttribute))]
+    public class ReadOnlyVarAttributeDrawer : PropertyDrawer {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            EditorGUI.BeginDisabledGroup(true);
+            //base.OnGUI(position, property, label);
+            _ = EditorGUI.PropertyField(position, property, label);
+            EditorGUI.EndDisabledGroup();
+        }
+    }
+}
